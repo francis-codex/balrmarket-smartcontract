de60e4ffbf3319606bd0a4a555e41e8c95c7f5b1
francis-codex <francislogo2219@gmail.com>
2025-07-20 16:49:07 +0100

    Feat: Initialization and Refactoring code structure for improved readability and maintainability

---
+++ programs/balrmarket/src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(unexpected_cfgs)]
+
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+
+declare_id!("EUF5zgyR7ZpKbx6kFqGr7FjGCevrCcDuWW9YJXpiuq8H");
+
+#[program]
+pub mod balrmarket {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
